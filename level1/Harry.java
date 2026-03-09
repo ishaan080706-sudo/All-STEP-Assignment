63fc61aecf41e30706c11fb16ab2920e9552f5e0
ishaan080706-sudo <ishaan080706@gmail.com>
2026-03-09 20:29:26 +0530

    Update Harry.java

--- level1/Harry.java
+++ level1/Harry.java
@@ -1,5 +1,7 @@
-public class Harry {
-    public static void main(String[] args) {
+public class Harry 
+{
+    public static void main(String[] args) 
+    {
         int birthYear = 2000;
         int currentYear = 2024;
 
